a41a2e09f3c7a731051f6ff69c60cd3fe05e0e99
Nagendra <nagendrayadav.k@gmail.com>
2025-04-08 23:56:11 +0530

    Assignments 1

---
+++ src/language/basics/Assignment1_DataTypes.java
@@ -0,0 +1,101 @@
+package language.basics;
+
+public class Assignment1_DataTypes {
+
+	public static void main(String[] args) {
+
+		// 1. Temperature of a city in degrees Celsius: 25.5 (DataType is float)
+		float celsiusVal = 25.5f;
+		System.out.println("Temperature of a city in degrees Celsius : " + celsiusVal);
+
+		// 2. Whether a customer has placed an order: true or false (DataType is boolean) output is true
+		boolean placedOrder = true;
+		System.out.println("Whether a customer has placed an order: " + placedOrder);
+		
+		// 2. Whether a customer has placed an order: true or false (DataType is boolean) output is false
+		boolean placedOrd = false;
+		System.out.println("Whether a customer has placed an order: " + placedOrd);
+		
+		// 2. Whether a customer has placed an order: true or false (DataType is boolean) output is true or false
+		System.out.println("Whether a customer has placed an order: " + placedOrder + " or "+placedOrd);
+		
+		// 3. Person's phone number (DataType is String)
+		String phoneNum ="123-456-7890";
+		System.out.println("Person's phone number: "+phoneNum);
+		
+		// 4. Amount of money in a customer's bank account: 1000.50 (DataType is float)
+		float amount = 1000.50f;
+		System.out.println("Amount of money in a customer's bank account: " + amount);
+		
+		//5. Person's email address: "john.doe@example.com" (DataType is String)
+		String emailAddress = "john.doe@example.com";
+		System.out.println("Person's email address: "+emailAddress);
+		
+		//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194 (DataType is float)
+		float latitudeLocation = 37.7749f;
+		float longitudeLocation = -122.4194f;
+		System.out.println("Coordinates of a location (latitude, longitude): "+latitudeLocation+", "+longitudeLocation);
+		
+		// 7. Person's marital status: true or false (DataType is boolean)
+		boolean maritalStatus = true;
+		System.out.println("Person's marital status: "+maritalStatus);
+		
+		// 8. Person's occupation: "Software Engineer" (DataType is String)
+		String personOccupation = "Software Engineer";
+		System.out.println("Person's occupation: "+personOccupation);
+		
+		//9. Person's favourite colour: "Blue" (DataType is String)
+		String favColour = "Blue";
+		System.out.println("Person's favourite colour: "+favColour);
+		
+		//10.Current year: 2023 (DataType is short)
+		short currentYear = 2023;
+		System.out.println("Current year: "+currentYear);
+		
+		//11.Number of followers on a social media platform: 1,000,000 (DataType is int)
+		int numOfFollowers = 1000000;
+		System.out.println("Number of followers on a social media platform: "+numOfFollowers);
+		
+		// 12.Rating of a movie: 7.5 (DataType is float)
+		float movieRating = 7.5f;
+		System.out.println("Rating of a movie: "+movieRating);
+		
+		// 13.Person's blood type: 'A' (DataType is char)
+		char bloodType = 'A';
+		System.out.println("Person's blood type: "+bloodType);
+		
+		// 13.Person's blood type: 'A' (DataType is char) assigning value is ASCII value
+		char bloodTypeAscii = 65;
+		System.out.println("Person's blood type: "+bloodTypeAscii);
+		
+		// 14.Title of a book: "To Kill a Mockingbird" (DataType is String)
+		String bookTitle = "To Kill a Mockingbird";
+		System.out.println("Title of a book: "+bookTitle);
+		
+		// 15.Number of employees in a company: 500 (DataType is short)
+		short noofEmployees = 500;
+		System.out.println("Number of employees in a company: "+noofEmployees);
+		
+		//16.Time of an event: 2:30 PM (DataType is String)
+		String eventTime= "2:30 PM";
+		System.out.println("Time of an event: "+eventTime);
+		
+		//17.Name of a country: "United States" (DataType is String)
+		String countryName= "India";
+		System.out.println("Name of a country: "+countryName);
+		
+		// 18.Person's eye color: "Brown" (DataType is String)
+		String eyeColor= "Brown";
+		System.out.println("Person's eye color: "+eyeColor);
+		
+		//19.Person's birthplace: "New York City" (DataType is String)
+		String birthPlace= "Proddatur";
+		System.out.println("Person's birthplace: "+birthPlace);
+		
+		//20.Distance between two cities: 200.5 (DataType is float)
+		float distanceTwoCities = 200.5f;
+		System.out.println("Distance between two cities: "+distanceTwoCities);
+
+	}
+
+}
