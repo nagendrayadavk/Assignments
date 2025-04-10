01e9572dd315572adff010592aeedf494d9c3a53
Nagendra <nagendrayadav.k@gmail.com>
2025-04-10 23:57:47 +0530

    Assignments 3

---
+++ src/assignments/Assignment3_EmpGroups.java
@@ -0,0 +1,15 @@
+package assignments;
+
+public class Assignment3_EmpGroups {
+
+	public static void main(String[] args) {
+		
+		Assignment3_Employees empData = new Assignment3_Employees();
+		
+		System.out.println("Employee Name: "+ empData.employeeName[0] + ", Employee ID: "+empData.employeeId[0]);
+		System.out.println("Employee Name: "+ empData.employeeName[1] + ", Employee ID: "+empData.employeeId[1]);
+		System.out.println("Employee Name: "+ empData.employeeName[2] + ", Employee ID: "+empData.employeeId[2]);
+
+	}
+
+}
---
+++ src/assignments/Assignment3_Employees.java
@@ -0,0 +1,13 @@
+package assignments;
+
+public class Assignment3_Employees {
+	
+	String [] employeeName = { "Nagendra", "Prasanna", "Lakshmi"};
+	int [] employeeId = {1234, 1235, 1236};
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
