a9262742ed523b552bcdb0e306de5cde705a4550
Nagendra <nagendrayadav.k@gmail.com>
2025-04-16 20:41:42 +0530

    Collections

--- src/assignments/Assignment3_EmpGroups.java
+++ src/assignments/Assignment3_EmpGroups.java
@@ -4,8 +4,10 @@ public class Assignment3_EmpGroups {
 
 	public static void main(String[] args) {
 		
+		//Create object of Employees class
 		Assignment3_Employees empData = new Assignment3_Employees();
 		
+		//Access the data and store in arrays and print the data
 		System.out.println("Employee Name: "+ empData.employeeName[0] + ", Employee ID: "+empData.employeeId[0]);
 		System.out.println("Employee Name: "+ empData.employeeName[1] + ", Employee ID: "+empData.employeeId[1]);
 		System.out.println("Employee Name: "+ empData.employeeName[2] + ", Employee ID: "+empData.employeeId[2]);
